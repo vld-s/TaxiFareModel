64d34c2f3b81070bd46ff34ad75ab7c9647fff1b
Vladimir Son <vladimir.son@outlook.com>
2021-08-31 16:31:08 +0100

    prg

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,64 @@
+# imports
+from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
+from TaxiFareModel.utils import compute_rmse
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
+        '''returns a pipelined model'''
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+        self.pipeline = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+
+    def run(self):
+        """set and train the pipeline"""
+        '''returns a trained pipelined model'''
+        
+        self.set_pipeline()
+        self.pipeline.fit(self.X, self.y)
+        return self.pipeline
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        '''returns the value of the RMSE'''
+
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+        #print(rmse)
+        return rmse
+
+
+if __name__ == "__main__":
+    # get data
+    # clean data
+    # set X and y
+    # hold out
+    # train
+    # evaluate
+    print('TODO')
